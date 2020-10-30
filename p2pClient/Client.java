3fc6ee26b086c16e3f502c26d955054149c41b55
wesmith340 <35306192+wesmith340@users.noreply.github.com>
2020-10-29 22:13:41 -0400

    Update Client.java

--- p2pClient/Client.java
+++ p2pClient/Client.java
@@ -8,6 +8,9 @@ package p2pClient;
 
 import Packet.Packet;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -18,6 +21,16 @@ import java.util.Scanner;
  * This class is designed to handle client side operations for a Peer-to-Peer style file-sharer
  */
 public class Client extends Thread{
+    final Font font1     = new Font("Calibri", Font.PLAIN, 20);
+    final Font messageFont = new Font("Courier New", Font.PLAIN, 20);
+    private final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+    private final int WIDTH = 1200;
+    private final int HEIGHT = 600;
+
+    private JFrame mainFrame;
+    private static JTextArea outputArea;
+    private JButton searchButton, listButton, quitButton;
+
     // Declarations
     protected int serverPort;
     protected InetAddress ip;
@@ -52,6 +65,59 @@ public class Client extends Thread{
      * @param serverPort
      */
     public Client(int peerID, int peerListenPort, char[] FILE_VECTOR, String ip, int serverPort) {
+        UIManager.put("OptionPane.messageFont", messageFont);
+        UIManager.put("OptionPane.buttonFont", messageFont);
+
+
+        mainFrame = new JFrame();
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        outputArea = new JTextArea(30,30);
+        outputArea.setEditable(false);
+        outputArea.setFont(messageFont);
+        outputArea.setBackground(Color.LIGHT_GRAY);
+        outputArea.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        mainPanel.add(outputArea);
+
+        mainFrame.add(mainPanel);
+
+        JPanel buttonPanel = new JPanel();
+        searchButton = new JButton("Search for file");
+        searchButton.setFont(font1);
+        searchButton.addActionListener(new ButtonHandler());
+        buttonPanel.add(searchButton);
+        listButton = new JButton("List Connections");
+        listButton.setFont(font1);
+        listButton.addActionListener(new ButtonHandler());
+        buttonPanel.add(listButton);
+        quitButton = new JButton("Quit");
+        quitButton.setFont(font1);
+        quitButton.addActionListener(new ButtonHandler());
+        buttonPanel.add(quitButton);
+        mainFrame.add(buttonPanel, BorderLayout.SOUTH);
+
+        mainFrame.setLocation((dim.width - WIDTH)/2, (dim.height - HEIGHT)/2);
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        WindowListener exitListener = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showOptionDialog(
+                        null, "Are you sure to want to close the connection?",
+                        "Exit Confirmation", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (confirm == 0) {
+                    closeConnection();
+                    System.exit(0);
+                }
+            }
+        };
+        mainFrame.addWindowListener(exitListener);
+        mainFrame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+
+
         this.peerID = peerID;
         this.peerListenPort = peerListenPort;
         this.FILE_VECTOR = FILE_VECTOR;
@@ -68,16 +134,17 @@ public class Client extends Thread{
             s = new Socket(ip, serverPort);
             DataInputStream inputStream = new DataInputStream(s.getInputStream());
             if (inputStream.readBoolean()) {
+                outputArea.append("Successfully connected to the server\n");
                 System.out.println("Successfully connected to the server");
                 connected = true;
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Unable to connect to the server");
+            System.exit(0);
         }
         // If unable to connect, shutdown the client
         if (!connected) {
-            System.out.println("Unable to connect to the server");
-            System.exit(0);
+            System.out.println("Unable to connect to server, not caught");
         }
     }
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -130,8 +197,10 @@ public class Client extends Thread{
      */
     private void fileLookupResult(int fileHolderId) {
         if (fileHolderId >= 0) {
+            outputArea.append("Client " + fileHolderId + " has the file\n");
             System.out.println("Client " + fileHolderId + " has the file");
         } else {
+            outputArea.append("No one has this file\n");
             System.out.println("No one has this file");
         }
     }
@@ -142,9 +211,10 @@ public class Client extends Thread{
         try {
             s.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
         }
         running = false;
+        outputArea.append("Connection Closed\n");
         System.out.println("Connection Closed");
         System.exit(0);
     }
@@ -163,6 +233,7 @@ public class Client extends Thread{
             try {
                 outputStream.writeObject(fileLookupPacket);
                 outputStream.flush();
+                outputArea.append("Sent packet\n");
                 System.out.println("Sent packet");
             } catch (IOException e) {
                 e.printStackTrace();
@@ -177,6 +248,7 @@ public class Client extends Thread{
      */
     public void askToClose() {
         try {
+            outputArea.append("Closing the Connection\n");
             System.out.println("Closing the Connection");
             running = false;
             Packet closePacket = new Packet();
@@ -208,6 +280,37 @@ public class Client extends Thread{
                 "Client ID: %d\nServer Port: %d\nClient Port: %d\nFile Vector: %s",
                 peerID, serverPort, peerListenPort, String.valueOf(FILE_VECTOR));
     }
+    // Handlers
+    private class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == searchButton) {
+                try {
+                    int input = Integer.parseInt(JOptionPane.showInputDialog("Please enter the index of the file you are looking for"));
+                    if (input >= 0 && input < 64) {
+                        fileLookup(input);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Please enter a number between 0 and 63");
+                    }
+                } catch (NumberFormatException  e) {
+                    JOptionPane.showMessageDialog(null, "Please enter a number between 0 and 63");
+                }
+            } else if (event.getSource() == listButton) {
+                outputArea.append(String.format(
+                        "Client ID: %d\nServer Port: %d\nClient Port: %d\nFile Vector: %s\n",
+                        peerID, serverPort, peerListenPort, String.valueOf(FILE_VECTOR))
+                );
+            } else if (event.getSource() == quitButton) {
+                int confirm = JOptionPane.showOptionDialog(
+                        null, "Are you sure to want to close the connection?",
+                        "Exit Confirmation", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (confirm == 0) {
+                    askToClose();
+                    System.exit(0);
+                }
+            }
+        }
+    }
 /*--------------------------------------------------------------------------------------------------------------------*/
     // Main
     /**
@@ -215,13 +318,44 @@ public class Client extends Thread{
      * @param args
      */
     public static void main(String args[]) {
+        //-----------------------------------------------------------------------
+        // File chooser
+        final Font font1     = new Font("Calibri", Font.PLAIN, 20);
+        final Font messageFont = new Font("Calibri", Font.PLAIN, 20);
+        final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        final int WIDTH  = 900, HEIGHT = 850;
+        UIManager.put("OptionPane.messageFont", messageFont);
+        UIManager.put("OptionPane.buttonFont", messageFont);
+
+        JFileChooser fc = new JFileChooser();
+        fc.setFont(font1);
+        fc.setCurrentDirectory(new File("."));
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize(new Dimension(WIDTH, HEIGHT));
+        frame.setLocation((dim.width - WIDTH)/2, (dim.height - HEIGHT)/2);
+        frame.setResizable(true);
+        File clientFile = null;
+        int returnVal = fc.showOpenDialog(frame);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            // Use JFileChooser to get a file
+            clientFile = fc.getSelectedFile();
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "ERROR: File not Found\nShutting down",
+                    "Error Message", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        //-----------------------------------------------------------------------
+        /*
         if (args.length < 2) {
             System.out.println("Usage: java Client Server_IP_Address Initialization_File_Name");
             System.exit(0);
         }
 
         File clientFile = new File(args[1]);
-
+        */
         int peerID = -1, serverPort = -1, peerListenPort = -1;
         char[] FILE_VECTOR = null;
         try {
@@ -246,13 +380,16 @@ public class Client extends Thread{
             System.out.println("Error: File not found\nSHUTTING DOWN");
             System.exit(0);
         }
+        //String serverIP = args[0];
+        String serverIP = JOptionPane.showInputDialog("Please enter the Server's IP address");
 
         // Instantiate the Client
-        Client client = new Client(peerID, peerListenPort, FILE_VECTOR, args[0], serverPort);
+        Client client = new Client(peerID, peerListenPort, FILE_VECTOR, serverIP, serverPort);
         System.out.println(client.toString());
 
         client.start();
 
+        /*
         //done! now loop for user input
         while (client.isRunning()){
             // User inputs
@@ -278,7 +415,7 @@ public class Client extends Thread{
             scanner.nextLine();
 
         }
-
+    */
     }
 /*--------------------------------------------------------------------------------------------------------------------*/
 }
